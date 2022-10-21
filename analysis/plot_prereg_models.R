d308ebe83febf14e22868a868d526ffbf0876b91
Luke Maurits <luke@maurits.id.au>
2022-10-21 16:30:09 +0200

    Save plots in dedicated directory, and fix consecutive vs simultaneous labelling.

--- analysis/plot_prereg_models.R
+++ analysis/plot_prereg_models.R
@@ -1,7 +1,6 @@
 library(tidyverse)
+library(scales)
 library(brms)
-
-interval_probs <- c(0.025, 0.975)
 
 # Model 1
 m <- read_rds("model1.rds")
@@ -30,23 +29,24 @@ ggplot(plot_table) +
 		  aes(x=x, y=y),
 		  linetype="dotted", color=colours[2], size=1)
 
-ggsave("model1_preds.png")
+ggsave("../plots/model1_preds.png")
 
 # Model 2
 m <- read_rds("model2.rds")
 
 nd <- filter(expand_grid(game_type=c("dyadic", "triadic"), consec=c(-0.5, 0, 0.5), session=8), (game_type == "dyadic" & consec==0) | (game_type == "triadic" & consec != 0))
-preds <- posterior_predict(m2, newdata=nd, re_formula=NA)
+preds <- posterior_predict(m, newdata=nd, re_formula=NA)
 plot_table <- tibble(game_type=character(),
 		     total_offer_prop=numeric())
 for(i in 1:nrow(preds)) {
 	plot_table <- add_row(plot_table, game_type="dyadic", total_offer_prop=preds[i,1])
-	plot_table <- add_row(plot_table, game_type="triadic_con", total_offer_prop=preds[i,2])
-	plot_table <- add_row(plot_table, game_type="triadic_sim", total_offer_prop=preds[i,3])
+	plot_table <- add_row(plot_table, game_type="triadic_sim", total_offer_prop=preds[i,2])
+	plot_table <- add_row(plot_table, game_type="triadic_con", total_offer_prop=preds[i,3])
 }
 
 dyadic_mean <- mean(preds[,1])
-triadic_mean <- mean(preds[,2])
+triadic_sim_mean <- mean(preds[,2])
+triadic_con_mean <- mean(preds[,3])
 colours <- hue_pal()(3)
 ggplot(plot_table) +
 	geom_density(aes(x=total_offer_prop,
@@ -56,8 +56,11 @@ ggplot(plot_table) +
 	geom_line(data=tibble(x=c(dyadic_mean, dyadic_mean), y=c(0,Inf)),
 		  aes(x=x, y=y),
 		  linetype="dotted", color=colours[1], size=1) +
-	geom_line(data=tibble(x=c(triadic_mean, triadic_mean), y=c(0,Inf)),
+	geom_line(data=tibble(x=c(triadic_con_mean, triadic_con_mean), y=c(0,Inf)),
 		  aes(x=x, y=y),
-		  linetype="dotted", color=colours[2], size=1)
+		  linetype="dotted", color=colours[2], size=1) +
+	geom_line(data=tibble(x=c(triadic_sim_mean, triadic_sim_mean), y=c(0,Inf)),
+		  aes(x=x, y=y),
+		  linetype="dotted", color=colours[3], size=1)
 
-ggsave("model2_preds.png")
+ggsave("../plots/model2_preds.png")
