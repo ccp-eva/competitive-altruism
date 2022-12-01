3db7a857ae0f7a2541ca07260b9e16238781f2f6
Luke Maurits <luke@maurits.id.au>
2022-12-01 14:04:18 +0100

    Explicitly remove trials with no previous winner from increasing dataset to avoid warnings from brms.

--- analysis/fit_increase_models.R
+++ analysis/fit_increase_models.R
@@ -4,6 +4,11 @@ library(brms)
 d <- read_csv("../data/increase_data.csv") %>%
 	filter(game_type == "triadic")
 
+# Discard trials where both offers on the previous trial were 0, resulting
+# in there being no "winner" against to measure increases.
+# (removes about 10% of trials)
+d <- filter(d, !is.na(increased_winner))
+
 m_self <- brm(increased_self ~ previous_accepted*session + (1|proposer_id),
 	      data=d, family="bernoulli", control=list(adapt_delta=0.95),
               prior = prior(normal(0, 1.5), class="b"))
