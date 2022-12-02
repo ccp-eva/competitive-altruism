c160d043e4b3399168facc8aa961fb2e93f9e141
Luke Maurits <luke@maurits.id.au>
2022-12-02 15:38:30 +0100

    Use consistent priors for Bernoulli models.

--- analysis/fit_consecutive_models.R
+++ analysis/fit_consecutive_models.R
@@ -4,10 +4,12 @@ library(brms)
 d <- read_csv("../data/consecutive_data.csv")
 
 m_responder_chooses_first <- brm(chose_first ~ 1 + (1|responder), data=d,
-				family="bernoulli")
+		family="bernoulli",
+		prior = prior(normal(0, 1.5), class="Intercept"))
 write_rds(m_responder_chooses_first, "chooses_first.rds")
 
 d$raises <- d$second_offer > d$first_offer
 m_second_proposer_raises <- brm(raises ~ 1 + (1|second_proposer), data=d,
-                                family="bernoulli")
+		family="bernoulli",
+		prior = prior(normal(0, 1.5), class="Intercept"))
 write_rds(m_second_proposer_raises, "second_raises.rds")
--- analysis/fit_final_vs_first_model.R
+++ analysis/fit_final_vs_first_model.R
@@ -5,7 +5,7 @@ d <- read_csv("../data/session_level_data.csv") %>%
 
 m <- brm(increased ~ 0 + game_type*session + (0 + game_type|triad_id),
 	 data=d, family="bernoulli",
-	 prior=prior(normal(-1, 1.5), class="b"),
+	 prior=prior(normal(0, 1.5), class="b"),
 	 cores=4, control=list(adapt_delta=0.95))
 
 write_rds(m, "first_last.rds")
