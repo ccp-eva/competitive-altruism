6a5d690fa60261bab6ac5f821b45a45954e412e4
Luke Maurits <luke@maurits.id.au>
2023-12-20 15:35:41 +0100

    Add more raw data plots to demonstrate consistency of behaviours across triads.

--- analysis/plot_raw_data_visualisations.R
+++ analysis/plot_raw_data_visualisations.R
@@ -51,3 +51,102 @@ d %>%
 
 ggsave("../plots/mean_offer_by_trial.png")
 
+# Emprical within session offer increasing behaviour
+
+d <- read_csv("../data/session_level_data.csv") %>%
+	mutate(increased = final_offer > first_offer) %>%
+	filter(session > 8)
+
+emp1 <- d %>% group_by(proposer_id, game_type) %>%
+	summarise(increase=mean(increased)) %>%
+	rename(Unit=proposer_id) %>%
+	mutate(role="Proposer")
+emp2 <- d %>% group_by(responder_id, game_type) %>%
+	summarise(increase=mean(increased)) %>%
+	rename(Unit=responder_id) %>%
+	mutate(role="Responder")
+emp3 <- d %>% group_by(triad_id, game_type) %>%
+	summarise(increase=mean(increased)) %>%
+	rename(Unit=triad_id) %>%
+	mutate(role="Triad")
+
+rbind(emp1, emp2, emp3) %>%
+	rename(`Game type` = game_type,
+	       `Proportion of sessions` = increase) %>%
+	ggplot() +
+	geom_bar(aes(x=Unit, y=`Proportion of sessions`, fill=`Game type`),
+		 stat="identity", position="dodge") +
+	geom_hline(yintercept=0.5, linetype="dotted") +
+	facet_wrap(~role, scales="free_x") +
+	ggtitle("Proportion of sessions 9-16 with final offer exceeding intial offer, grouped by proposer, responder or triad")
+
+ggsave("../plots/empirical_increasing_within_session.png")
+
+# Empirical increasing relative to self behaviour
+
+d <- read_csv("../data/increase_data.csv") %>%
+	filter(game_type == "triadic",
+	       session > 8)
+
+emp1 <- d %>% group_by(proposer_id, previous_accepted, game_type) %>%
+	summarise(increase=mean(increased_self)) %>%
+	rename(Unit=proposer_id) %>%
+	mutate(role="Proposer")
+emp2 <- d %>% group_by(responder_id, previous_accepted, game_type) %>%
+	summarise(increase=mean(increased_self)) %>%
+	rename(Unit=responder_id) %>%
+	mutate(role="Responder")
+emp3 <- d %>% group_by(triad_id, previous_accepted, game_type) %>%
+	summarise(increase=mean(increased_self)) %>%
+	rename(Unit=triad_id) %>%
+	mutate(role="Triad")
+
+rbind(emp1, emp2, emp3) %>%
+	rename(`Previous offer accepted` = previous_accepted,
+	       `Proportion of trials` = increase) %>%
+	ggplot() +
+	geom_bar(aes(x=Unit, y=`Proportion of trials`, fill=`Previous offer accepted`),
+		 stat="identity", position="dodge") +
+	geom_hline(yintercept=0.5, linetype="dotted") +
+	facet_wrap(~role, scales="free_x") +
+	ggtitle("Proportion of trials in sessions 9-16 where proposer offered more than in previous trial, grouped by proposer, responder and triad")
+
+ggsave("../plots/empirical_increasing_over_self.png")
+
+# Empirical outbidding behaviour
+
+d <- read_csv("../data/consecutive_data.csv") %>%
+	filter(session > 8,
+	       first_offer <= 4) %>%
+	mutate(match  = second_offer >= first_offer,
+	       outbid = second_offer >  first_offer)
+
+emp1 <- d %>% group_by(second_proposer_id) %>%
+	summarise(`Match or outbid`=mean(match),
+		  `Strictly outbid`=mean(outbid)) %>%
+	rename(Unit=second_proposer_id) %>%
+	mutate(role="Proposer")
+emp2 <- d %>% group_by(responder_id) %>%
+	summarise(`Match or outbid`=mean(match),
+		  `Strictly outbid`=mean(outbid)) %>%
+	rename(Unit=responder_id) %>%
+	mutate(role="Responder")
+emp3 <- d %>% group_by(triad_id) %>%
+	summarise(`Match or outbid`=mean(match),
+		  `Strictly outbid`=mean(outbid)) %>%
+	rename(Unit=triad_id) %>%
+	mutate(role="Triad")
+
+rbind(emp1, emp2, emp3) %>%
+	pivot_longer(cols = c("Match or outbid", "Strictly outbid"),
+		     names_to = "Criterion",
+		     values_to = "prop") %>%
+	rename(`Proportion of first offers` = prop) %>%
+	ggplot() +
+	geom_bar(aes(x=Unit, y=`Proportion of first offers`, fill=Criterion),
+		 stat="identity", position="dodge") +
+	geom_hline(yintercept=0.5, linetype="dotted") +
+	facet_wrap(~role, scales="free_x") +
+	ggtitle("Proportion of first offers of 4 grapes or fewer in sessions 9-16 which were matched or outbid by the second proposer, grouped by proposer, responder and triad")
+
+ggsave("../plots/empirical_outbidding.png")
